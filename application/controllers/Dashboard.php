02cad4d8dd167bb2a5e8e0602de0fac0f2a49789
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2024-11-11 20:22:58 +0700

    read data

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,7 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
-	public function index(){
+	public function index()
+  {
     $data['title'] = 'Dashboard';
 
     $this->load->view('template/header', $data);
